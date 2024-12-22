626056bd8ac7980d5e6e0e25c390f98660e1f5d1
xmik <xmik2022@gmail.com>
2024-12-22 22:42:43 +0300

    Revert "[for next completing] changed setup_db"
    This reverts commit 0bfce55c6a543cb3a3cad86aee656a49fcfc1163.

--- setup_db.sql
+++ setup_db.sql
@@ -1,8 +1,7 @@
 -- Создание таблицы Users
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE,
-    surname VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL CHECK (role IN ('student', 'instructor', 'admin')),
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -13,9 +12,9 @@ CREATE TABLE Users (
 CREATE TABLE Courses (
     course_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
+    description TEXT,
     instructor_id INTEGER NOT NULL,
-    start_time TIMESTAMP NOT NULL,
-    duration INTERVAL NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (instructor_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
