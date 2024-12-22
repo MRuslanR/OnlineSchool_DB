0bfce55c6a543cb3a3cad86aee656a49fcfc1163
Ruslan <141405635+MRuslanR@users.noreply.github.com>
2024-12-22 22:41:04 +0300

    [for next completing] changed setup_db

--- setup_db.sql
+++ setup_db.sql
@@ -1,7 +1,8 @@
 -- Создание таблицы Users
 CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    surname VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL CHECK (role IN ('student', 'instructor', 'admin')),
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -12,9 +13,9 @@ CREATE TABLE Users (
 CREATE TABLE Courses (
     course_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description TEXT,
     instructor_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    start_time TIMESTAMP NOT NULL,
+    duration INTERVAL NOT NULL,
     FOREIGN KEY (instructor_id) REFERENCES Users(user_id) ON DELETE CASCADE
 );
 
